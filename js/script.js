ef80590b17dafd398f2b5f6570d496a671577dc0
Tenin Sidime <sidime.tenin@gmail.com>
2019-03-27 21:41:45 +0000

    first save

--- js/script.js
+++ js/script.js
@@ -4,8 +4,20 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+    var searchTerm = $("searchButton").val();
+    var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response){
+                var pic_url = response.data[5]
+                .image.original.url;
+                $("body").attribute("<img src =" + pic_url + ">");
+               console.log(response)
+      }
+      
+  });
+
   
 });
 
